8af49e66c43eed96874bbf71637a26470ccc3361
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-12 20:07:29 +0700

    03. Create a Contact Page: 0302_Validation

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,21 +3,29 @@
 
 @section('content')
 
-    <form action="" method="post">
+    @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+    <form action="{{ route('contact.submit') }}" method="post">
+        {{ csrf_field() }}
         <div class="form-group">
             <label for="name">Name</label>
-            <input type="text" class="form-control" id="name" name="name">
+            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
         </div>
 
         <div class="form-group">
             <label for="email">E-mail</label>
-            <input type="text" class="form-control" id="email" name="email">
+            <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}">
         </div>
 
         <div class="form-group">
             <label for="message">Message</label>
-            <textarea class="form-control" id="message" name="message" cols="30" rows="10">
-        </textarea>
+            <textarea class="form-control" id="message" name="message" cols="30" rows="10">{{ old('message') }}</textarea>
         </div>
 
         <button class="btn btn-primary">Send Message</button>
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,32 @@ Route::get('/', [ 'as' => 'home', function () {
     return view('home');
 }]);
 
+
 Route::get('about', [ 'as' => 'about', function () {
     return view('about');
 }]);
 
+
 Route::get('contact', [ 'as' => 'contact.form', function () {
     return view('contact');
+}]);
+
+
+Route::post('contact', ['as' => 'contact.submit', function () {
+
+    $validation = validator(
+        request()->only('name', 'email', 'message'),
+            [
+                'name' => 'required',
+                'email' => 'required|email',
+                'message' => 'required'
+            ]
+    );
+
+    if ($validation->passes()) {
+        dd('hooray it passed');
+    }
+
+    return redirect()->route('contact.form')->withErrors($validation->errors())->withInput();
+
 }]);
